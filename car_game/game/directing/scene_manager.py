8e631399049c3c2be239e3e9600a3bb9f9cd6d7e
rodrigoloyo <rmloyo@byui.edu>
2022-03-19 12:03:03 -0600

    actor and car

--- car_game/game/directing/scene_manager.py
+++ car_game/game/directing/scene_manager.py
@@ -1,36 +1,36 @@
-import csv
-from constants import *
-from game.casting.animation import Animation
-from game.casting.ball import Ball
-from game.casting.body import Body
-from game.casting.brick import Brick
-from game.casting.image import Image
-from game.casting.label import Label
-from game.casting.point import Point
-from game.casting.racket import Racket
-from game.casting.stats import Stats
-from game.casting.text import Text 
-from game.scripting.change_scene_action import ChangeSceneAction
-from game.scripting.check_over_action import CheckOverAction
-from game.scripting.collide_borders_action import CollideBordersAction
-from game.scripting.collide_brick_action import CollideBrickAction
-from game.scripting.collide_racket_action import CollideRacketAction
-from game.scripting.control_racket_action import ControlRacketAction
-from game.scripting.draw_ball_action import DrawBallAction
-from game.scripting.draw_bricks_action import DrawBricksAction
-from game.scripting.draw_dialog_action import DrawDialogAction
-from game.scripting.draw_hud_action import DrawHudAction
-from game.scripting.draw_racket_action import DrawRacketAction
-from game.scripting.end_drawing_action import EndDrawingAction
-from game.scripting.initialize_devices_action import InitializeDevicesAction
-from game.scripting.load_assets_action import LoadAssetsAction
-from game.scripting.move_ball_action import MoveBallAction
-from game.scripting.move_racket_action import MoveRacketAction
-from game.scripting.play_sound_action import PlaySoundAction
-from game.scripting.release_devices_action import ReleaseDevicesAction
-from game.scripting.start_drawing_action import StartDrawingAction
-from game.scripting.timed_change_scene_action import TimedChangeSceneAction
-from game.scripting.unload_assets_action import UnloadAssetsAction
+# import csv
+# from constants import *
+# from game.casting.animation import Animation
+# from game.casting.ball import Ball
+# from game.casting.body import Body
+# from game.casting.brick import Brick
+# from game.casting.image import Image
+# from game.casting.label import Label
+# from game.casting.point import Point
+# from game.casting.racket import Racket
+# from game.casting.stats import Stats
+# from game.casting.text import Text 
+# from game.scripting.change_scene_action import ChangeSceneAction
+# from game.scripting.check_over_action import CheckOverAction
+# from game.scripting.collide_borders_action import CollideBordersAction
+# from game.scripting.collide_brick_action import CollideBrickAction
+# from game.scripting.collide_racket_action import CollideRacketAction
+# from game.scripting.control_racket_action import ControlRacketAction
+# from game.scripting.draw_ball_action import DrawBallAction
+# from game.scripting.draw_bricks_action import DrawBricksAction
+# from game.scripting.draw_dialog_action import DrawDialogAction
+# from game.scripting.draw_hud_action import DrawHudAction
+# from game.scripting.draw_racket_action import DrawRacketAction
+# from game.scripting.end_drawing_action import EndDrawingAction
+# from game.scripting.initialize_devices_action import InitializeDevicesAction
+# from game.scripting.load_assets_action import LoadAssetsAction
+# from game.scripting.move_ball_action import MoveBallAction
+# from game.scripting.move_racket_action import MoveRacketAction
+# from game.scripting.play_sound_action import PlaySoundAction
+# from game.scripting.release_devices_action import ReleaseDevicesAction
+# from game.scripting.start_drawing_action import StartDrawingAction
+# from game.scripting.timed_change_scene_action import TimedChangeSceneAction
+# from game.scripting.unload_assets_action import UnloadAssetsAction
 from game.services.raylib.raylib_audio_service import RaylibAudioService
 from game.services.raylib.raylib_keyboard_service import RaylibKeyboardService
 from game.services.raylib.raylib_physics_service import RaylibPhysicsService
